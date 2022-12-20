d3bd8c3678312f481f859b0033d10a67c2e7a6fa
KoldaevAlexey <koldaev.alexey@gmail.com>
2022-12-20 23:37:04 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+import { HomeScreen } from "./screens/HomeScreen";
+import { AboutScreen } from "./screens/AboutScreen";
+import { CatalogScreen } from "./screens/CatalogScreen";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName;
+                        if (route.name === "Главная") {
+                            iconName = focused
+                                ? "information-circle"
+                                : "information-circle-outline";
+                        } else if (route.name === "Еще") {
+                            iconName = focused ? "list" : "list-outline";
+                        } else if (route.name === "Каталог") {
+                            iconName = focused ? "bicycle" : "bicycle-outline";
+                        }
+                        return (
+                            <Ionicons
+                                name={iconName}
+                                size={size}
+                                color={color}
+                            />
+                        );
+                    },
+                    tabBarActiveTintColor: "tomato",
+                    tabBarInactiveTintColor: "gray",
+                })}
+            >
+                <Tab.Screen name="Главная" component={HomeScreen} />
+                <Tab.Screen name="Каталог" component={CatalogScreen} />
+                <Tab.Screen name="Еще" component={AboutScreen} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/AboutScreen.js
@@ -0,0 +1,22 @@
+import { View, Text } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
+
+const AboutScreen = () => {
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false,
+        });
+    }, []);
+
+    return (
+        <SafeAreaView className="bg-blue-500 h-full p-5">
+            <Text>AboutScreen</Text>
+        </SafeAreaView>
+    );
+};
+
+export { AboutScreen };
